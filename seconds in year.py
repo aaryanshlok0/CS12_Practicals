36111ddd4a6c6669cf45173de1a6c13a51ea7712
Aaryan <134126135+aaryanshlok0@users.noreply.github.com>
2025-03-16 12:18:52 +0530

    Update seconds in year.py

--- seconds in year.py
+++ seconds in year.py
@@ -1,8 +1,8 @@
 #program that prints no of seconds in a year
-year=int(input("1. Common year\n2. Leap year: "))
-if year==1:
-    print(f"No of seconds in a year is {24*60*60*365}")
-elif year==2:
-    print(f"No of seconds in a leap year is {24*60*60*366}")
+year = int(input("1. Common year:\n2. Leap year:\nEnter your choice: "))
+if year == 1:
+    print(f"Number of seconds in a year: {24 * 60 * 60 * 365}")
+elif year == 2:
+    print(f"Number of seconds in a leap year: {24 * 60 * 60 * 366}")
 else:
-    raise ValueError("Inavlid input!Please try again")
+    raise ValueError("Invalid input! Please try again.")
