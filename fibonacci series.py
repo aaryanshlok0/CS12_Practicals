00868c430a72693d782624d674613f6e5fa2a5c9
Aaryan <134126135+aaryanshlok0@users.noreply.github.com>
2025-03-16 12:56:47 +0530

    Update fibonacci series.py

--- fibonacci series.py
+++ fibonacci series.py
@@ -1,20 +1,6 @@
 #this program prints the first n numbers of fibonacci series
-#using for loop
-
-a,b=0,1
-n=int(input("Enter the no of terms you want fibonacci for: "))
-fibonacci=(a,)
-if n<=0:
-    raise ValueError("Please input a number greater than zero.")
-elif n>0:
-    for i in range(1,n+1):
-        fibonacci+=(b,)
-        a,b=b,a+b
-print(fibonacci)
-
-
-
 #using while loop
+
 a,b=0,1
 fibonacci=(a,)
 n=int(input("Enter no of terms you want for fibonacci series: ")) 
@@ -27,5 +13,3 @@ elif n>0:
         a,b=b,a+b
         count-=1
     print(fibonacci)
-
-#using binets formula:
